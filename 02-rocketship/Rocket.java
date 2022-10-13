a527d2071d0da0aaa8d1070ef081f7cda1fe2b21
davidt389 <114939067+davidt389@users.noreply.github.com>
2022-10-13 12:08:06 -0400

    Add files via upload

---
+++ 02-rocketship/Rocket.java
@@ -0,0 +1,35 @@
+
+/**
+ * Write a description of class Rocket here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+//author David Tian
+public class Rocket
+{
+    public static void main(String[]args) {
+     top();
+     second();
+     tried();
+    }
+    
+    public static void top() {
+        System.out.println("   /\\ ");
+        System.out.println("  /  \\");
+        System.out.println(" /    \\");
+    }
+    
+    public static void second() {
+        System.out.println("+------+");
+        System.out.println("|      |");
+        System.out.println("|      |");
+        System.out.println("+------+");
+    }
+    
+    public static void tried() {
+        System.out.println("|UNITED|");
+        System.out.println("|STATES|");
+    }
+
+}
